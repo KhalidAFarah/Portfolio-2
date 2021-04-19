2e05dc983073924e1a181f9c1d0688b283f2abea
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-04-19 15:57:36 +0200

    venter på portfolio 2 pdfen

--- Server.py
+++ Server.py
@@ -5,9 +5,31 @@ app = Flask(__name__)
 
 api = Api(app)
 
+#Rendering the base site
 @app.route("/")
 def renders_site():
     return render_template("index.html")
+
+#status for restAPI
+@app.route("/status", methods=['GET'])
+def status():
+    response = jsonify({"Status": "Ok"})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+class item(Resource):
+    def get(self): # get an item from the db
+        pass
+class itemPost(Resource):
+    def post(self): # add an item to the db (admin)
+        pass
+
+
+api.add_resource(item, "/item/<int:itemId>/")
+api.add_resource(itemPost, "/item/")
+    
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
