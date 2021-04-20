fc25c85ab27d4e373fb1894e9b39d9a9b24cdf20
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-04-20 13:42:00 +0200

    post and get user done?

--- Server.py
+++ Server.py
@@ -14,11 +14,6 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-mycursor.execute("SHOW TABLES")
-for x in mycursor:
-    print(x)
-
-
 #Rendering the base site
 @app.route("/")
 def renders_site():
@@ -31,15 +26,44 @@ def status():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+
+class UserGetAll(Resource):
+    def get(self):
+        mycursor.execute("SELECT * FROM Customers")
+        response = []
+        for user  in mycursor:       
+            response.append({"user_id": user[0],"Username":user[1],"Password":user[2],"Email":user[3]})
+        response = jsonify(response)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
 class UserGet(Resource):
-    def get(self, user_id):
-        pass
+    def get(self, Username, Password):
+        mycursor.execute("SELECT * FROM Customers WHERE Username = \"{}\" AND Password = \"{}\"".format(Username, Password))
+        for user  in mycursor:       
+            response = {"user_id": user[0],"Username":user[1],"Password":user[2],"Email":user[3]}
+            response = jsonify(response)
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            return response
+
+        abort(404, message="User not found")
+
 class UserPost(Resource):
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument("Username")
+        parser.add_argument("Password")
+        parser.add_argument("Email")
+        data = parser.parse_args()
 
-api.add_resource(UserGet, "/user/<int:user_id>/")
+        mycursor.execute("INSERT INTO Customers (Username, Password, Email) VALUES ({}, {}, {})".format(data['Username'], data['Password'], data['Email']))
+        db.commit()
+
+        return 200
+
+api.add_resource(UserGet, "/user/<Username>/<Password>/")
 api.add_resource(UserPost, "/user/")
+api.add_resource(UserGetAll, "/users/")
 
 class item(Resource):
     def get(self, item_id): # get an item from the db
@@ -56,6 +80,5 @@ api.add_resource(itemPost, "/item/")
 
     
 if __name__ == "__main__":
-    #app.run(debug=True)
-    pass
+    app.run(debug=True)
 
--- portfolio_2_mysql_file.sql
+++ portfolio_2_mysql_file.sql
@@ -2,12 +2,10 @@ CREATE DATABASE IF NOT EXISTS Webshop;
 USE Webshop;
 CREATE TABLE IF NOT EXISTS Customers (
 Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Username VARCHAR(50) NOT NULL,
-Password VARCHAR(50) NOT NULL,
+Username VARCHAR(50) NOT NULL UNIQUE,
+Password VARCHAR(50) NOT NULL UNIQUE,
 Email VARCHAR(70)
 );
- 
-SHOW TABLES;
 
 /*CREATE USER  IF NOT EXISTS "general"@"localhost" IDENTIFIED WITH mysql_native_password BY "general";
 GRANT ALL privileges ON Webshop . * TO "general"@"localhost";*/
